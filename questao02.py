ecad7d3c43055869eb960534438ed713b6e6d2c4
Luk4ss <lucasregoaraujo@gmail.com>
2022-03-21 23:51:03 -0300

    :tada: atividade finalizada

--- questao02.py
+++ questao02.py
@@ -2,6 +2,10 @@
 precoProduto = float(input("Digite o preço do produto: R$ ").strip())
 
 # Preço com desconto 
-precoProduto = 0.9 *precoProduto
+preco_com_desconto = 0.9 *precoProduto
 
-print("O preço do produto com 10 porcento de desconto é igual a: R$ %.2f" %(precoProduto))
+print(preco_com_desconto)
+preco_com_desconto = round(preco_com_desconto, 2)
+
+
+print("O preço do produto com 10 porcento de desconto é igual a: R$",(preco_com_desconto))
