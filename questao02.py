f420bb573633b8c9528fa8962a9313fe2d6c6a86
Luk4ss <lucasregoaraujo@gmail.com>
2022-03-21 23:51:27 -0300

    :tada: atividade finalizada

--- questao02.py
+++ questao02.py
@@ -4,7 +4,7 @@ precoProduto = float(input("Digite o preço do produto: R$ ").strip())
 # Preço com desconto 
 preco_com_desconto = 0.9 *precoProduto
 
-print(preco_com_desconto)
+
 preco_com_desconto = round(preco_com_desconto, 2)
 
 
